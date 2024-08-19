97f9a88fad39a4a44f9edbe622e0cb8811971bf8
Youssef <70177128+joeamroo@users.noreply.github.com>
2024-08-19 00:46:48 -0500

    Add files via upload

---
+++ test.cpp
@@ -0,0 +1,10 @@
+// test.cpp
+
+
+int main() {
+    int var = 0; // This line contains a tab character.
+    int var2 = 0; // This line contains spaces.
+    int mixed = 0; // This line contains mixed tabs and spaces.
+    int trailing = 0;    // This line contains trailing whitespace.
+    return 0;
+}
